96b78a9714e9a628fe2098f00eda31b8fe6664b9
Christopher Mark Gore <cgore@cgore.com>
2020-07-08 23:03:05 -0500

    Adding a grass tile

--- Assets/Source/Mono Behaviors/GameController.cs
+++ Assets/Source/Mono Behaviors/GameController.cs
@@ -7,6 +7,8 @@ public class GameController : MonoBehaviour
 {
     public Grid grid;
     public Tilemap tilemap;
+
+    public Tile grassTile;
     public Tile waterTile;
 
     public void Resize(int x, int y)
@@ -14,11 +16,17 @@ public class GameController : MonoBehaviour
         tilemap.size = new Vector3Int(x, y, 1);
     }
 
+    public void SetTile(int x, int y, Tile tile)
+    {
+        if (0 <= x && x < tilemap.size.x && 0 <= y && y < tilemap.size.y)
+            tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+    }
+
     public void FillWithTile(Tile tile)
     {
         for (int x = 0; x < tilemap.size.x; x++)
             for (int y = 0; y < tilemap.size.y; y++)
-                tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                SetTile(x, y, tile);
     }
 
     // Start is called before the first frame update
@@ -26,6 +34,9 @@ public class GameController : MonoBehaviour
     {
         Resize(10, 10);
         FillWithTile(waterTile);
+        SetTile(0, 0, grassTile);
+        SetTile(1, 1, grassTile);
+        SetTile(2, 2, grassTile);
     }
 
     // Update is called once per frame
