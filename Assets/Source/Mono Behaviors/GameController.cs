f85db26ef567f7f8cf3b2f42cc43ac7478207b14
Christopher Mark Gore <cgore@cgore.com>
2020-07-08 21:49:25 -0500

    set the tilemap size to 10x10 initially

--- Assets/Source/Mono Behaviors/GameController.cs
+++ Assets/Source/Mono Behaviors/GameController.cs
@@ -8,9 +8,17 @@ public class GameController : MonoBehaviour
     public Grid grid;
     public Tilemap tilemap;
 
+    public void Resize(int x, int y)
+    {
+        Debug.Log("tilemap size was " + tilemap.size);
+        tilemap.size = new Vector3Int(x, y, 1);
+        Debug.Log("tilemap size is now " + tilemap.size);
+    }
+
     // Start is called before the first frame update
     void Start()
-    { 
+    {
+        Resize(10, 10);
     }
 
     // Update is called once per frame
